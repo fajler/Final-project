4e7e048c8e644d2eb0fba7f8d91b82e05dee86a5
Fajle Rabbi Hamdu <mdfajlerabbi4122@gmail.com>
2024-12-07 20:27:48 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
+
+
+
+
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          montagu: ['"Montagu Slab"', 'serif'], // Add Montagu Slab as a custom font
+        },
+      },
+    },
+    plugins: [],
+  };
